64b5da03310247fffb43c0e1f6bbad8fa1669b08
Reese Lee <48657837+reese-lee@users.noreply.github.com>
2019-09-20 15:22:48 -0700

    add files and folders for client

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { apiResponse: "" };
+    }
+
+    callAPI() {
+        fetch("http://localhost:9000/testAPI")
+            .then(res => res.text())
+            .then(res => this.setState({ apiResponse: res }))
+            .catch(err => err);
+    }
+
+    componentDidMount() {
+        this.callAPI();
+    }
+
+    render() {
+        return (
+            <div>
+                <header>
+                    <h1 className="App-title">Log In Here</h1>
+                </header>
+                <p>{this.state.apiResponse}</p>
+            </div>
+        );
+    }
+}
+
+export default App;
